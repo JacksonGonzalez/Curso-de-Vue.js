55398f01ba652419348e90b8dae53273f8189994
JacksonGonzalez <jaesgoca@gmail.com>
2018-04-30 22:55:23 -0500

    Configurando la API de Lasfm

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '6ddd1d5270c75335b183fd4eae55da31',
+    secret: '71b44d4e504b400c2e6190ec60440703',
+    registeredTo: 'BGGonzalez11'
+}
+
+// Application name	platzimusic
+// API key	6ddd1d5270c75335b183fd4eae55da31
+// Shared secret	71b44d4e504b400c2e6190ec60440703
+// Registered to	BGGonzalez11
